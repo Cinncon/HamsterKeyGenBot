f1648c84af0089ad5fd820dd1bd63c53b3a3c64a
Cinncon <can301423@gmail.com>
2024-08-23 12:16:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
             appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
             promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
             eventsDelay: 20000,
-            attemptsNumber: 12,
+            attemptsNumber: 15,
             
 	},
         9: {
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
             appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
             promoId: '8814a785-97fb-4177-9193-ca4180ff9da8',
             eventsDelay: 20000,
-            attemptsNumber: 12,
+            attemptsNumber: 15,
             
 	}
     };
