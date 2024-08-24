ea67a9a31d7b1ed14171a54d611ce1b52be624c2
Cinncon <can301423@gmail.com>
2024-08-24 09:39:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,42 +8,42 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Riding Extreme 3D',
             appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
             promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
-            eventsDelay: 30000,
+            eventsDelay: 21000,
             attemptsNumber: 25,
         },
         2: {
             name: 'Chain Cube 2048',
             appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
             promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
-            eventsDelay: 30000,
+            eventsDelay: 21000,
             attemptsNumber: 20
         },
         3: {
             name: 'My Clone Army',
             appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
             promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
-            eventsDelay: 150000,
+            eventsDelay: 121000,
             attemptsNumber: 30,
         },
         4: {
             name: 'Train Miner',
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
-            eventsDelay: 30000,
-            attemptsNumber: 15,
+            eventsDelay: 21000,
+            attemptsNumber: 20,
         },
         5: {
             name: 'Merge Away',
             appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
             promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
-            eventsDelay: 30000,
-            attemptsNumber: 25,
+            eventsDelay: 21000,
+            attemptsNumber: 20,
         },
         6: {
             name: 'Twerk Race 3D',
             appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
             promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-            eventsDelay: 30000,
+            eventsDelay: 21000,
             attemptsNumber: 20,
             
         },
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Polysphere',
             appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
             promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
-            eventsDelay: 20000,
+            eventsDelay: 21000,
             attemptsNumber: 16,
             
 	},
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Mow and Trim',
             appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
             promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
-            eventsDelay: 20000,
+            eventsDelay: 21000,
             attemptsNumber: 15,
             
 	},
@@ -67,7 +67,7 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Mud Racing ',
             appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
             promoId: '8814a785-97fb-4177-9193-ca4180ff9da8',
-            eventsDelay: 20000,
+            eventsDelay: 21000,
             attemptsNumber: 15,
             
 	}
