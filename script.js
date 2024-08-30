b49fd903bcdc6a239eaf7cec025661a7132ac699
Cinncon <can301423@gmail.com>
2024-08-30 12:08:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const games = {
         1: {
-            name: 'Riding Extreme 3D',
-            appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
-            promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
+            name: 'ZooPolis',
+            appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+            promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
             eventsDelay: 21000,
-            attemptsNumber: 25,
+            attemptsNumber: 20,
         },
         2: {
             name: 'Chain Cube 2048',
@@ -57,9 +57,9 @@ document.addEventListener('DOMContentLoaded', () => {
             
 	},
         9: {
-            name: 'Mud Racing ',
-            appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
-            promoId: '8814a785-97fb-4177-9193-ca4180ff9da8',
+            name: 'Gangs Wars',
+            appToken: 'xxxx',
+            promoId: 'xxxx',
             eventsDelay: 21000,
             attemptsNumber: 15,
             
