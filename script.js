4de98c287faf88b0b937fb63417bb21eb4c88c91
Cinncon <can301423@gmail.com>
2024-09-04 20:10:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
             appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
             promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
             eventsDelay: 21000,
-            attemptsNumber: 20,
+            attemptsNumber: 20
         },
         2: {
             name: 'Chain Cube 2048',
@@ -23,21 +23,21 @@ document.addEventListener('DOMContentLoaded', () => {
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
             eventsDelay: 21000,
-            attemptsNumber: 20,
+            attemptsNumber: 20
         },
         5: {
             name: 'Merge Away',
             appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
             promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
             eventsDelay: 21000,
-            attemptsNumber: 20,
+            attemptsNumber: 20
         },
         6: {
             name: 'Twerk Race 3D',
             appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
             promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
             eventsDelay: 21000,
-            attemptsNumber: 20,
+            attemptsNumber: 20
             
         },
         7: {
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
             appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
             promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
             eventsDelay: 21000,
-            attemptsNumber: 16,
+            attemptsNumber: 16
             
 	},
         8: {
@@ -53,24 +53,24 @@ document.addEventListener('DOMContentLoaded', () => {
             appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
             promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
             eventsDelay: 21000,
-            attemptsNumber: 15,
+            attemptsNumber: 15
             
 	},
         9: {
-            name: 'TileTrio',
+            name: 'Tile Trio',
             appToken: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
             promoId: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
             eventsDelay: 15000,
-            attemptsNumber: 16,
+            attemptsNumber: 16
             
 	},
         3: {
-            name: `FluffCrusade',
+            name: `Fluff Crusade',
             appToken: '112887b0-a8af-4eb2-ac63-d82df78283d9',
             promoId: '112887b0-a8af-4eb2-ac63-d82df78283d9',
             eventsDelay: 40000,
-            attemptsNumber: 20,
-        },
+            attemptsNumber: 20
+        }
     };
 
 
